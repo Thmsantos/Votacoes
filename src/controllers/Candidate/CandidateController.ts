657713333598fb69cb683ef9d1f52a099fbaa529
Thmsantos <thiagomessias411@gmail.com>
2025-12-11 15:38:51 -0300

    🎨 style: ident files

--- src/controllers/Candidate/CandidateController.ts
+++ src/controllers/Candidate/CandidateController.ts
@@ -5,67 +5,67 @@ import type GetCandidatesService from "../../services/GetCandidatesService/GetCa
 import type GetResultsService from "../../services/GetResults/GetResultsService";
 
 export default class CandidateController {
-  private createCandidateService;
+    private createCandidateService;
 
-  private getCandidatesService;
+    private getCandidatesService;
 
-  private getResultsService;
+    private getResultsService;
 
-  constructor(
-    createCandidateService: CreateCandidateService,
-    getCandidatesService: GetCandidatesService,
-    getResultsService: GetResultsService,
-  ) {
-    this.createCandidateService = createCandidateService;
-    this.getCandidatesService = getCandidatesService;
-    this.getResultsService = getResultsService;
-  }
+    constructor(
+        createCandidateService: CreateCandidateService,
+        getCandidatesService: GetCandidatesService,
+        getResultsService: GetResultsService,
+    ) {
+        this.createCandidateService = createCandidateService;
+        this.getCandidatesService = getCandidatesService;
+        this.getResultsService = getResultsService;
+    }
 
-  public async create(ctx: Context<{ body: CandidateBody }>) {
-    try {
-      await this.createCandidateService.execute(ctx.body, ctx.request);
+    public async create(ctx: Context<{ body: CandidateBody }>) {
+        try {
+            await this.createCandidateService.execute(ctx.body, ctx.request);
 
-      ctx.set.status = 201;
-      return { message: "created" };
-    } catch (error) {
-      if (error instanceof Error) {
-        if (error.message === "UNAUTHORIZED") {
-          ctx.set.status = 401;
-          return { message: "Unauthorized" };
+            ctx.set.status = 201;
+            return { message: "created" };
+        } catch (error) {
+            if (error instanceof Error) {
+                if (error.message === "UNAUTHORIZED") {
+                    ctx.set.status = 401;
+                    return { message: "Unauthorized" };
+                }
+
+                if (error.message === "USER_EXISTS") {
+                    ctx.set.status = 409;
+                    return { message: "User already exists" };
+                }
+            }
+
+            ctx.set.status = 500;
+            return { message: "Internal error" };
         }
+    }
 
-        if (error.message === "USER_EXISTS") {
-          ctx.set.status = 409;
-          return { message: "User already exists" };
+    public async list(ctx: Context) {
+        try {
+            const candidates = await this.getCandidatesService.execute();
+
+            ctx.set.status = 200;
+            return candidates;
+        } catch {
+            ctx.set.status = 500;
+            return { message: "Internal error" };
         }
-      }
-
-      ctx.set.status = 500;
-      return { message: "Internal error" };
     }
-  }
 
-  public async list(ctx: Context) {
-    try {
-      const candidates = await this.getCandidatesService.execute();
+    public async results(ctx: Context) {
+        try {
+            const results = await this.getResultsService.execute();
 
-      ctx.set.status = 200;
-      return candidates;
-    } catch {
-      ctx.set.status = 500;
-      return { message: "Internal error" };
+            ctx.set.status = 200;
+            return results;
+        } catch {
+            ctx.set.status = 500;
+            return { message: "Internal error" };
+        }
     }
-  }
-
-  public async results(ctx: Context) {
-    try {
-      const results = await this.getResultsService.execute();
-
-      ctx.set.status = 200;
-      return results;
-    } catch {
-      ctx.set.status = 500;
-      return { message: "Internal error" };
-    }
-  }
 }
--- src/repositories/Candidates/CandidateRepository.ts
+++ src/repositories/Candidates/CandidateRepository.ts
@@ -1,34 +1,34 @@
 import type { PrismaClient } from "@prisma/client";
 
 export default class CandidateRepository {
-  private db: PrismaClient;
+    private db: PrismaClient;
 
-  constructor(db: PrismaClient) {
-    this.db = db;
-  }
+    constructor(db: PrismaClient) {
+        this.db = db;
+    }
 
-  public async findByName(name: string) {
-    return this.db.candidate.findFirst({
-      where: { name },
-    });
-  }
+    public async findByName(name: string) {
+        return this.db.candidate.findFirst({
+            where: { name },
+        });
+    }
 
-  public async create(name: string) {
-    return this.db.candidate.create({
-      data: { name, votes: 0 },
-    });
-  }
+    public async create(name: string) {
+        return this.db.candidate.create({
+            data: { name, votes: 0 },
+        });
+    }
 
-  public async findMany() {
-    return this.db.candidate.findMany();
-  }
+    public async findMany() {
+        return this.db.candidate.findMany();
+    }
 
-  public async findResults() {
-    console.log("bate aq?", await this.db.candidate.findMany());
+    public async findResults() {
+        console.log("bate aq?", await this.db.candidate.findMany());
 
-    return this.db.candidate.findMany({
-      select: { name: true, votes: true },
-      orderBy: { votes: "desc" },
-    });
-  }
+        return this.db.candidate.findMany({
+            select: { name: true, votes: true },
+            orderBy: { votes: "desc" },
+        });
+    }
 }
