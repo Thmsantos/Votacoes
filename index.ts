225785f2f71044481292b02d741799041526f4c2
Thmsantos <thiagomessias411@gmail.com>
2025-12-22 16:44:35 -0300

    :sparkles: feat: add user routes

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { Elysia } from "elysia";
 import swagger from "@elysiajs/swagger";
-
 import { candidateRoutes } from "./src/presentation/http/routes/candidateRoutes";
 import { voteRoutes } from "./src/presentation/http/routes/voteRoutes";
+import { userRoutes } from "./src/presentation/http/routes/userRoutes";
 
-new Elysia().use(swagger()).use(candidateRoutes).use(voteRoutes).listen(3200);
+new Elysia()
+    .use(swagger())
+    .use(candidateRoutes)
+    .use(voteRoutes)
+    .use(userRoutes)
+    .listen(3200);
 
 console.log("Server running at http://127.0.0.1:3200");
