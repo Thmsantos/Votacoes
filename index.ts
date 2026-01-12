ea9489917572de1e686e928f26bb79f59de122ae
Thmsantos <thiagomessias411@gmail.com>
2026-01-12 12:36:24 -0300

    :bug: fix: remove keys of data

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { candidateRoutes } from "./src/presentation/http/routes/candidateRoutes"
 import { voteRoutes } from "./src/presentation/http/routes/voteRoutes";
 import { userRoutes } from "./src/presentation/http/routes/userRoutes";
 import { authRoutes } from "./src/presentation/http/routes/authRoutes";
+import { electionsRoutes } from "./src/presentation/http/routes/electionsRoutes";
 
 new Elysia()
     .use(swagger())
@@ -11,6 +12,7 @@ new Elysia()
     .use(voteRoutes)
     .use(userRoutes)
     .use(authRoutes)
+    .use(electionsRoutes)
     .listen(3200);
 
 console.log("Server running at http://127.0.0.1:3200");
